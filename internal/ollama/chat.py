8eb1f8e5d13b90dd73955ec15ad4fb17cd4d566d
Nahuel Luca <nahuelluca20@gmail.com>
2024-11-24 15:00:28 -0300

    Update .gitignore and refactor chat_ollama function to include context handling in main.py

--- internal/ollama/chat.py
+++ internal/ollama/chat.py
@@ -1,21 +1,13 @@
-from ollama import ChatResponse
 import ollama
 
 
-def chat_ollama(diff, question, model_name):
-    messages = [
-        {
-            "role": "system",
-            "content": f"Analyze the following git diff and summarize the differences between the branches. Git diff content:\n{diff}",
-        },
-        {"role": "user", "content": question},
-    ]
+def chat_ollama(diff, question, model_name, llm_context):
     context = f"Based in this git diff info: {diff}"
     prompt_for_ollama = context + f" Provide a response to: {question}"
     generation_response = ollama.generate(
-        model=model_name,
-        prompt=prompt_for_ollama,
+        model=model_name, prompt=prompt_for_ollama, context=llm_context
     )
-
+    context = generation_response["context"]
     response_text = generation_response["response"].strip()
-    print(response_text)
+
+    return response_text, context
--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from internal.git.git_diff import get_git_diff
 from internal.ollama.chat import chat_ollama
-
-# qwen2.5-coder:7b
-
-
-# def RunModel():
-#     diff = git_diff("main", "feat/git-proof")
-#     chat(diff, "What is the difference between these branches", "qwen2.5-coder:7b")
-
-
-# RunModel()
+from rich.console import Console
+from rich.markdown import Markdown
 
 
 def run_model():
+    console = Console()
     diff = get_git_diff("main", "feat/git-proof")
-    chat_ollama(diff, "Are there repeated functions? How many?", "qwen2.5-coder:7b")
+
+    console.print(
+        "[bold green]Welcome to the interactive chat! Type your question or '/bye' to exit.[/bold green]"
+    )
+    llm_context = []
+
+    while True:
+        question = console.input("[bold blue]Ask your question: [/bold blue]")
+        if question.lower() == "/bye":
+            console.print("[bold red]Goodbye![/bold red]")
+            break
+        response_text, context = chat_ollama(
+            diff, question, "qwen2.5-coder:7b", llm_context
+        )
+        llm_context = context
+        console.print(Markdown(response_text))
 
 
 if __name__ == "__main__":
